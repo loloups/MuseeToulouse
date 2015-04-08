88d1ed75c65a395b7260da090cfc6385cb8542c9
loloups <loic.leger@univ-tlse3.fr>
2015-04-08 15:12:39 +0200

    Merge branch 'master' of https://github.com/loloups/MuseeToulouse
    Conflicts:
    	application.properties
    	grailsw

--- grails-app/domain/museetoulouse/Adresse.groovy
+++ grails-app/domain/museetoulouse/Adresse.groovy
@@ -2,6 +2,15 @@ package museetoulouse
 
 class Adresse {
 
+    int numero
+    String rue
+    int codePostal
+    String ville
+
+    static belongsTo = [musee : Musee]
+
     static constraints = {
+        codePostal size:5,matches: "[0-9]+"
+        ville matches: "[A-Z]+"
     }
 }
--- grails-app/domain/museetoulouse/Musee.groovy
+++ grails-app/domain/museetoulouse/Musee.groovy
@@ -1,7 +1,16 @@
 package museetoulouse
 
 class Musee {
+    String nom
+    String horairesOuverture
+    String telephone
+    String accesMetro
+    String accesBus
+
+    Gestionnaire responsable
+    Adresse adresse
 
     static constraints = {
+        telephone matches: "d{10}"
     }
 }
