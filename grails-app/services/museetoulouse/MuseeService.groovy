47c3147eb6a0616536001b6d438a0f14dbf40281
Loic LEGER <loicleger@MacBook-Pro-de-Loic.local>
2015-04-15 14:11:01 +0200

    Moteur de recherche

---
+++ grails-app/services/museetoulouse/MuseeService.groovy
@@ -0,0 +1,39 @@
+package museetoulouse
+
+import grails.transaction.Transactional
+
+@Transactional
+class MuseeService {
+
+    def serviceMethod() {
+
+    }
+
+    /**
+     * Cherche les musees correspondant aux critères
+     * @param inNomMusee chaîne de caractère présente dans le nom du musee
+     * @param inCodePostal chaîne de caractère presente dans le codepostal
+     * @param inNomRue chaîne de caractère presente dans la rue du musee
+     * @return
+     */
+    List<Musee> searchMusees(String inNomMusee, String inCodePostal, String inNomRue) {
+        def criteria = Musee.createCriteria()
+        List<Musee> res = criteria.list {
+            if (inNomMusee) {
+                    like 'nom', "%${inNomMusee}%"
+            }
+            if (inCodePostal) {
+                adresse {
+                        like 'codePostal', "${inCodePostal}"
+                }
+            }
+            if (inNomRue) {
+                adresse {
+                    like 'rue', "%${inNomRue}%"
+                }
+            }
+            order('nom')
+        }
+        res
+    }
+}
